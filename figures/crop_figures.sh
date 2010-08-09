e88e28882af72f9d1796f8b7e0005aed74f5eee5
Richard Lincoln <r.w.lincoln@gmail.com>
2010-08-09 13:27:27 +0100

    Adding chapter 5 discussion section.

--- figures/crop_figures.sh
+++ figures/crop_figures.sh
@@ -1,1 +1,10 @@
-pdfcrop fig5_1_g1_action.pdf fig5_1_g1_action.pdf
+#!/bin/bash
+pdfcrop --margins '0 0 0 0' fig5_1_action_a1.pdf fig5_1_action_a1.pdf
+pdfcrop --margins '0 0 0 0' fig5_1_action_a2.pdf fig5_1_action_a2.pdf
+
+pdfcrop --margins '0 0 0 0' fig5_2_action_a1.pdf fig5_2_action_a1.pdf
+pdfcrop --margins '0 0 0 0' fig5_2_action_a2.pdf fig5_2_action_a2.pdf
+pdfcrop --margins '0 0 0 0' fig5_2_reward_a1.pdf fig5_2_reward_a1.pdf
+pdfcrop --margins '0 0 0 0' fig5_2_reward_a2.pdf fig5_2_reward_a2.pdf
+
+pdfcrop --margins '0 0 26 0' ieee_rts_profiles.pdf ieee_rts_profiles.pdf
