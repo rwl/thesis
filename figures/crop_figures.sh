0f1ebfae3cdaca4fe9376ccef4133fba85a380b3
Richard Lincoln <r.w.lincoln@gmail.com>
2010-08-25 13:46:48 +0100

    Adding plots for ex6.1.

--- figures/crop_figures.sh
+++ figures/crop_figures.sh
@@ -7,6 +7,11 @@ pdfcrop --margins '0 0 0 0' fig5_2_action_a2.pdf fig5_2_action_a2.pdf
 pdfcrop --margins '0 0 0 0' fig5_2_reward_a1.pdf fig5_2_reward_a1.pdf
 pdfcrop --margins '0 0 0 0' fig5_2_reward_a2.pdf fig5_2_reward_a2.pdf
 
+pdfcrop --margins '0 0 26 0' fig6_1_reward_a1.pdf fig6_1_reward_a1.pdf
+pdfcrop --margins '0 0 26 0' fig6_1_reward_a2.pdf fig6_1_reward_a2.pdf
+pdfcrop --margins '0 0 26 0' fig6_1_reward_a3.pdf fig6_1_reward_a3.pdf
+pdfcrop --margins '0 0 26 0' fig6_1_reward_a4.pdf fig6_1_reward_a4.pdf
+
 pdfcrop --margins '0 0 26 0' ieee_rts_profiles.pdf ieee_rts_profiles.pdf
 
 pdfcrop ngt_grid.pdf ngt_grid.pdf
