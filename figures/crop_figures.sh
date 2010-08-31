29c03455ac09fed78dc3d5c6ee7efee530594d60
Richard Lincoln <r.w.lincoln@gmail.com>
2010-08-31 16:08:22 +0100

    Resizing figures.

--- figures/crop_figures.sh
+++ figures/crop_figures.sh
@@ -3,14 +3,12 @@ pdfcrop --margins '0 0 0 0' fig5_1_action_a1.pdf fig5_1_action_a1.pdf
 pdfcrop --margins '0 0 0 0' fig5_1_action_a2.pdf fig5_1_action_a2.pdf
 
 pdfcrop --margins '0 0 0 0' fig5_2_action_a1.pdf fig5_2_action_a1.pdf
-pdfcrop --margins '0 0 0 0' fig5_2_action_a2.pdf fig5_2_action_a2.pdf
 pdfcrop --margins '0 0 0 0' fig5_2_reward_a1.pdf fig5_2_reward_a1.pdf
-pdfcrop --margins '0 0 0 0' fig5_2_reward_a2.pdf fig5_2_reward_a2.pdf
 
-pdfcrop --margins '0 0 26 0' fig6_1_reward_a1.pdf fig6_1_reward_a1.pdf
-pdfcrop --margins '0 0 26 0' fig6_1_reward_a2.pdf fig6_1_reward_a2.pdf
-pdfcrop --margins '0 0 26 0' fig6_1_reward_a3.pdf fig6_1_reward_a3.pdf
-pdfcrop --margins '0 0 26 0' fig6_1_reward_a4.pdf fig6_1_reward_a4.pdf
+pdfcrop --margins '0 0 26 0' fig6_1.pdf fig6_1.pdf
+pdfcrop --margins '0 0 26 0' fig6_2_a1.pdf fig6_2_a1.pdf
+pdfcrop --margins '0 0 26 0' fig6_2_a4.pdf fig6_2_a4.pdf
+pdfcrop --margins '0 0 26 0' fig6_3.pdf fig6_3.pdf
 
 pdfcrop --margins '0 0 26 0' ieee_rts_profiles.pdf ieee_rts_profiles.pdf
 
