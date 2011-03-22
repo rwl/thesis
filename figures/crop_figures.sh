84e6bae25f15bb1db793270fbe536cefec2e8073
Richard Lincoln <r.w.lincoln@gmail.com>
2011-03-22 21:15:30 +0000

    Updating chapter 5 figures, adding x-axis labels and legends.

--- figures/crop_figures.sh
+++ figures/crop_figures.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#pdfcrop --margins '0 0 0 0' fig5_1_action_a1.pdf fig5_1_action_a1.pdf
-#pdfcrop --margins '0 0 0 0' fig5_1_action_a2.pdf fig5_1_action_a2.pdf
+pdfcrop --margins '0 0 0 0' fig5_1_action_a1.pdf fig5_1_action_a1.pdf
+pdfcrop --margins '0 0 0 0' fig5_1_action_a2.pdf fig5_1_action_a2.pdf
 
-#pdfcrop --margins '0 0 0 0' fig5_2_action_a1.pdf fig5_2_action_a1.pdf
-#pdfcrop --margins '0 0 0 0' fig5_2_reward_a1.pdf fig5_2_reward_a1.pdf
+pdfcrop --margins '0 0 0 0' fig5_2_action_a1.pdf fig5_2_action_a1.pdf
+pdfcrop --margins '0 0 0 0' fig5_2_reward_a1.pdf fig5_2_reward_a1.pdf
 
 #pdfcrop --margins '0 0 26 0' fig6_1.pdf fig6_1.pdf
 #pdfcrop --margins '0 0 26 0' fig6_2_a1.pdf fig6_2_a1.pdf
@@ -13,5 +13,5 @@
 
 #pdfcrop --margins '0 0 26 0' ieee_rts_profiles.pdf ieee_rts_profiles.pdf
 
-pdfcrop ngt_grid.pdf ngt_grid.pdf
+#pdfcrop ngt_grid.pdf ngt_grid.pdf
 #pdfcrop ngt_gen.pdf ngt_gen.pdf
