ca66c6a22a8e6885bf793affa0cf32c39e3751f6
Richard Lincoln <r.w.lincoln@gmail.com>
2011-03-23 02:18:19 +0000

    Adding results for learning rate variations in chapter 5.

--- figures/crop_figures.sh
+++ figures/crop_figures.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-pdfcrop --margins '0 0 0 0' fig5_1_action_a1.pdf fig5_1_action_a1.pdf
-pdfcrop --margins '0 0 0 0' fig5_1_action_a2.pdf fig5_1_action_a2.pdf
+#pdfcrop --margins '0 0 0 0' fig5_1_action_a1.pdf fig5_1_action_a1.pdf
+#pdfcrop --margins '0 0 0 0' fig5_1_action_a2.pdf fig5_1_action_a2.pdf
 
-pdfcrop --margins '0 0 0 0' fig5_2_action_a1.pdf fig5_2_action_a1.pdf
-pdfcrop --margins '0 0 0 0' fig5_2_reward_a1.pdf fig5_2_reward_a1.pdf
+#pdfcrop --margins '0 0 0 0' fig5_2_action_a1.pdf fig5_2_action_a1.pdf
+#pdfcrop --margins '0 0 0 0' fig5_2_reward_a1.pdf fig5_2_reward_a1.pdf
+
+pdfcrop --margins '0 0 0 0' fig5_3_action_a2.pdf fig5_3_action_a2.pdf
+pdfcrop --margins '0 0 0 0' fig5_4_action_a2.pdf fig5_4_action_a2.pdf
 
 #pdfcrop --margins '0 0 26 0' fig6_1.pdf fig6_1.pdf
 #pdfcrop --margins '0 0 26 0' fig6_2_a1.pdf fig6_2_a1.pdf
